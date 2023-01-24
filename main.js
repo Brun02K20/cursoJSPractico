f4db3470f591ab3d333cf9f07838919ebe5e76d3
Bruno Virinni <bvirinni@gmail.com>
2023-01-24 11:45:40 -0300

    Primer cambio 24 de Enero de 2023

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ let menuCarIcon = document.querySelector(".navbar-shopping-cart");
 let shopCart = document.querySelector(".product-detail");
 
 
+// SELECCIONO EL CARDS-CONTAINER Y EL MAIN-CONTAINER DE LA LISTA DE PRODUCTOS DE LA PAGINA PRINCIPAL, LA LISTA DE PRODUCTOS QUE ESTA DISEÑADA CON CSS GRID
+let productCardsContainer = document.querySelector(".cards-container");
+let mainContainer = document.querySelector(".main-container")
+
 // AÑADO UN ESCUCHADOR PARA INDICARLE QUE CUANDO SE HAGA CLICK SOBRE EL EMAIL DE LA NAVBAR SE EJECUTE LA FUNCION TOGGLEDESKTOPMENU; MISMA LOGICA PARA EL MENU MOBILE
 emailMenu.addEventListener("click", toggleDesktopMenu);
 burgerIcon.addEventListener("click", toggleMobileMenu);
@@ -68,3 +72,116 @@ function toggleShoppingCart(){
 
     // LA LOGICA DE ESTA FUNCION APLICA A LAS FUNCIONES TOGGLEMOBILEMENU Y TOGGLEDESKTOPMENU
 }
+
+// VAMOS A HACER REUTILIZACION DE CODIGO PARA GENERAR LOS PRODUCTOS EN LA PAGINA PRINCIPAL; VAMOS A MAQUETAR LOS PRODUCTOS DESDE JS PARA NO TENER CADA PUTO PRODUCTO EN LOS HTML, ES INSANO Y LOCO XDDDDD
+
+// LO PRIMERO ES CREAR UN ARRAY DE PRODUCTOS, ES DECIR, CADA PRODUCTO ES UN OBJETO POR LO TANTO NECESITAREMOS UN ARRAY DE OBJETOS
+let productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Car",
+    price: 20000,
+    image: "https://images.pexels.com/photos/1637859/pexels-photo-1637859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Game of garden",
+    price: 450,
+    image: "https://images.pexels.com/photos/2647714/pexels-photo-2647714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Black Chair",
+    price: 30,
+    image: "https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Desk lamp",
+    price: 120,
+    image: "https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Macbook Air",
+    price: 3500,
+    image: "https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push({
+    name: "Set of ties",
+    price: 200,
+    image: "https://images.pexels.com/photos/130855/pexels-photo-130855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+// AHORA LO QUE TOCA ES INTRODUCIR ESE ARRAY DE OBJETOS EN NUESTRO HTML; PARA ESO DEBO RECORRER ESE ARRAY MEDIANTE UN FOR
+
+// Y PARA RECORRER ARRAYS APARTE DEL TIPICO FOR(I=0;I<ARR.LENGTH;I++) TENEMOS EL FOR (I OF ARRAYNAME); EN CADA RECORRIDO DEL CICLO PRODUCT SERA IGUAL AL OBJETO DE TURNO PERTENECIENTE AL ARRAY PRODUCT LIST
+
+
+/* 
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="product-img">
+    <div class="product-info">
+        <div>
+            <p>$120.00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>   
+</div> 
+*/
+
+// EN DEFINITIVA, ESTE FOR RECORRE EL ARREGLO DE PRODUCT LIST, MAQUETA LA PRODUCT CARD CORRESPONDIENTE Y LA MUESTRA POR PANTALLA AUTOMATICAMENTE 
+
+function renderPorducts(arr){
+    for (product of arr){
+
+        // ACA RECREAMOS LO QUE ESTA EN EL CODIGO HTML DE ARRIBA, PERO FALTARIA UNIR TODOS ESOS ELEMENTOS, SE HACE MAS ABAJO
+        let productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        let product_Image = document.createElement("img");
+        product_Image.classList.add("product-img");
+        product_Image.setAttribute("src", product.image);
+    
+        let productInformation = document.createElement("div");
+        productInformation.classList.add("product-info");
+    
+        let productDiv = document.createElement("div");
+    
+        let productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+    
+        let productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        let productFigure = document.createElement("figure");
+        let add_To_Cart_Button = document.createElement("img");
+        add_To_Cart_Button.setAttribute("src", "icons/bt_add_to_cart.svg");
+    
+    
+        // ACA VAMOS UNIENDO TODAS LAS COSAS
+        productFigure.appendChild(add_To_Cart_Button);
+        productDiv.append(productPrice,productName);
+        productInformation.append(productDiv,productFigure);
+        productCard.append(product_Image,productInformation);
+    
+        // YA CREAMOS LA TARJETITA DEL PRODUCTO, LO QUE NOS FALTA ES AÑADIR ESA TARJETITA AL CARDS-CONTAINER Y ESE CARDS-CONTAINER AL MAIN-CONTAINER
+    
+        productCardsContainer.append(productCard);
+    };
+};
+
+
+renderPorducts(productList);
+
+
+// EL FOR (PRODUCT IN PRODUCT) LO QUE VA A HACER ES QUE TE IMPRIME LA POSICION DEL PRODUCTO EN EL ARRAY
